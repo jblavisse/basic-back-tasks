9b66d7a48964a22a82f07744c064e1be592beee7
jblavisse <jean-baptiste@jblavisse.fr>
2021-07-12 15:08:51 +0200

    Add cors

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const jsonServer = require('json-server')
 const auth = require('json-server-auth')
  
 const app = jsonServer.create()
+const cors = require('cors');
+
+
 const router = jsonServer.router('db.json')
 
 const rules = auth.rewriter({
@@ -14,6 +17,15 @@ const rules = auth.rewriter({
 app.db = router.db
  
 // You must apply the auth middleware before the router
+app.use(
+  cors({
+      origin: true,
+      credentials: true,
+      preflightContinue: false,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  })
+);
+app.options('*', cors());
 app.use(rules)
 app.use(auth)
 app.use(router)
